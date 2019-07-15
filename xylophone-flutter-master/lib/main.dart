67899fa8162fb60923285bb419d091724c858388
mo3sw <mo3sw95@gmail.com>
2019-07-15 19:15:57 +0300

    Added my first button and it triggers a sound

---
+++ xylophone-flutter-master/lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
+import 'package:audioplayers/audio_cache.dart';
+
+void main() => runApp(XylophoneApp());
+
+class XylophoneApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+          child: Center(
+            child: FlatButton(
+              color: Colors.blue,
+                onPressed: () {
+                  AudioCache player = AudioCache();
+                  player.play('note1.wav');
+                },
+                child: Text('Click me')),
+          ),
+        ),
+      ),
+    );
+  }
+}
