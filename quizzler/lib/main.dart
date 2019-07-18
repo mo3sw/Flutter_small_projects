234ded64b29318416db95927d699b3baea827c3b
mo3sw <mo3sw95@gmail.com>
2019-07-18 23:19:02 +0300

    Finished with the quiz app

--- quizzler/lib/main.dart
+++ quizzler/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 //TODO: Step 2 - Import the rFlutter_Alert package here.
+import 'package:rflutter_alert/rflutter_alert.dart';
 import 'quiz_brain.dart';
 
 QuizBrain quizBrain = QuizBrain();
@@ -40,21 +41,33 @@ class _QuizPageState extends State<QuizPage> {
       //HINT! Step 4 Part B is in the quiz_brain.dart
       //TODO: Step 4 Part C - reset the questionNumber,
       //TODO: Step 4 Part D - empty out the scoreKeeper.
-
-      //TODO: Step 5 - If we've not reached the end, ELSE do the answer checking steps below 👇
-      if (userPickedAnswer == correctAnswer) {
-        scoreKeeper.add(Icon(
-          Icons.check,
-          color: Colors.green,
-        ));
-      } else {
-        scoreKeeper.add(Icon(
-          Icons.close,
-          color: Colors.red,
-        ));
+      if(quizBrain.isFinished()){
+        Alert(
+            context: context,
+            title: "Test has finished",
+            desc: "You are awesome",
+            style: AlertStyle(
+              animationDuration: Duration(milliseconds: 1000),
+            ),
+        ).show();
+        quizBrain.reset();
+        scoreKeeper.removeRange(0, scoreKeeper.length);
+      } else{
+        //TODO: Step 5 - If we've not reached the end, ELSE do the answer checking steps below 👇
+        if (userPickedAnswer == correctAnswer) {
+          scoreKeeper.add(Icon(
+            Icons.check,
+            color: Colors.green,
+          ));
+        } else {
+          scoreKeeper.add(Icon(
+            Icons.close,
+            color: Colors.red,
+          ));
+        }
+        quizBrain.nextQuestion();
       }
-      quizBrain.nextQuestion();
-    });
+      });
   }
 
   @override
--- quizzler/lib/quiz_brain.dart
+++ quizzler/lib/quiz_brain.dart
@@ -46,8 +46,20 @@ class QuizBrain {
   }
 
   //TODO: Step 3 Part A - Create a method called isFinished() here that checks to see if we have reached the last question. It should return (have an output) true if we've reached the last question and it should return false if we're not there yet.
-
   //TODO: Step 3 Part B - Use a print statement to check that isFinished is returning true when you are indeed at the end of the quiz and when a restart should happen.
-
+  bool isFinished(){
+    var length = _questionBank.length;
+    if(_questionNumber+1 == _questionBank.length){
+      print('Question#: $_questionNumber\nQuestion length: $length\nReturn Value: true\n');
+      return true;
+    } else{
+      print('Question#: $_questionNumber\nQuestion length: $length\nReturn Value: false\n');
+      return false;
+    }
+  }
   //TODO: Step 4 Part A - Create a reset() method here that sets the questionNumber back to 0.
+  void reset(){
+    _questionNumber = 0;
+  }
+
 }
