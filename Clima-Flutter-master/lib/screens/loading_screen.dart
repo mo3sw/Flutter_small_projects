64b5979bd84a1ac9275dad02153a9e267a2bdd74
mo3sw <mo3sw95@gmail.com>
2019-07-26 06:37:03 +0300

    Clima: Finished getting the device location

--- Clima-Flutter-master/lib/screens/loading_screen.dart
+++ Clima-Flutter-master/lib/screens/loading_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -6,13 +7,23 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+
+  // Getting the location of the device
+  // Added lines to ask for permission to access the location
+  // Android File: android/app/src/main/AndroidManifest.xml
+  // iOS File: ios/Runner/Info.plist
+  void getLocation() async{
+    Position position = await Geolocator().getCurrentPosition(desiredAccuracy: LocationAccuracy.low);
+    print(position);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: RaisedButton(
           onPressed: () {
-            //Get the current location
+            getLocation();
           },
           child: Text('Get Location'),
         ),
